9ca1d5fefde04c205e63ad3ac5dc1abec49948b5
orangehuang <b07902077@csie.ntu.edu.tw>
2021-12-06 19:05:53 +0800

    first commit

--- src/map.js
+++ src/map.js
@@ -45,7 +45,7 @@ var classGotoMap = function (mapOptions) {
         layers: accessibleLayers,
         target: mapSettiings.mapTarget,
         view: new ol.View({
-            center: ol.proj.fromLonLat([121.5, 24.85]),
+            center: ol.proj.fromLonLat([121.2, 24.95]),
             zoom: 10
           })
     });
