fe3a67df587c30139f348ed6b5bbc947e3d7c88a
orangehuang <b07902077@csie.ntu.edu.tw>
2021-12-08 02:01:22 +0800

    update

---
+++ village_src/map.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Epulari T on 3/16/2017.
+ */
+
+var map;
+var classGotoMap = function (mapOptions) {
+    var mapSettiings = $.extend({
+        mapTarget: 'map',
+        mapSouce: new ol.source.OSM(),
+        mapCenter: [0, 0],
+        mapZoom: 2
+    }, mapOptions);
+
+    var accessibleSource = mapSettiings.mapSouce;
+    var accessibleLayers = [
+        new ol.layer.Tile({
+            source: accessibleSource
+        })
+    ];
+    var accessibleView = new ol.View({
+        center: mapSettiings.mapCenter,
+        zoom: mapSettiings.mapZoom
+    });
+    var themap = new ol.Map({
+        layers: accessibleLayers,
+        target: mapSettiings.mapTarget,
+        view: new ol.View({
+            center: ol.proj.fromLonLat([121.1, 24.95]),
+            zoom: 10
+          })
+    });
+    map = themap;
+};
