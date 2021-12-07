6514bd79101cac8f496f3e3e3c5351380f37dc44
orangehuang <b07902077@csie.ntu.edu.tw>
2021-12-07 18:08:42 +0800

    update

--- src/function.js
+++ src/function.js
@@ -20,6 +20,7 @@ for (i = 0; i < data.length; i++) {
     console.log(c);
     var len = c[0].length + c[1].length + c[2].length + c[3].length;
     var get = [c[0], parseFloat(c[1]), parseFloat(c[2]), parseInt(c[3]), data[i].substr(len + 4)];
+    console.log('get');
     console.log(get);
     cdata.push(get)
 }
