298352d73bf7adabc3eecf712ce0245f75c33a1b
orangehuang <b07902077@csie.ntu.edu.tw>
2021-12-07 22:01:16 +0800

    update

--- src/map.js
+++ src/map.js
@@ -25,7 +25,7 @@ var classGotoMap = function (mapOptions) {
         layers: accessibleLayers,
         target: mapSettiings.mapTarget,
         view: new ol.View({
-            center: ol.proj.fromLonLat([122, 24.95]),
+            center: ol.proj.fromLonLat([121.2, 24.95]),
             zoom: 10
           })
     });
