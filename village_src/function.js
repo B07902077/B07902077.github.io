3fbf167ef93ebe0f754f6dbdaffface980c45e8b
orangehuang <b07902077@csie.ntu.edu.tw>
2022-01-05 00:10:36 +0800

    map

--- village_src/function.js
+++ village_src/function.js
@@ -130,7 +130,7 @@ for (i = 0; i < cdata.length; i++) {
                     element: container,
                     autoPan: true,
                     autoPanAnimation: {
-                        duration: 150
+                        duration: 100
                     },
                     offset: [1, -1]
                 }));
